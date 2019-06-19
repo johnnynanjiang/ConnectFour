5ecb56b0d58a3ac2160996671616ba452c18a431
Nan Jiang <johnny.nan.jiang@gmail.com>
2019-06-19 22:54:00 +0800

    Implement basic Bot behaviour

--- src/main/kotlin/io/github/johnnynanjiang/glamcorner/controller/GameController.kt
+++ src/main/kotlin/io/github/johnnynanjiang/glamcorner/controller/GameController.kt
@@ -3,6 +3,7 @@ package io.github.johnnynanjiang.glamcorner.controller
 import io.github.johnnynanjiang.glamcorner.model.*
 import io.github.johnnynanjiang.glamcorner.view.BoardView
 import java.lang.IllegalArgumentException
+import java.lang.Math.random
 
 class GameController {
     private companion object {
@@ -11,16 +12,17 @@ class GameController {
         const val MINIMUM_ROW_INDEX = 0
         const val MAXIMUM_ROW_INDEX = 5
 
-        const val HELP_TEXT = """
+        const val TEXT_HELP = """
 - type in column number straight away, e.g. 1
 - type q to quit the game
 - type h for this help
 """
-        const val QUIT_TEXT = "Quit the game!"
+        const val TEXT_QUIT = "Quit the game!"
 
         const val ERROR_INVALID_COLUMN = "Input should be a valid integer between 0 and %s"
         const val ERROR_COLUMN_NUMBER_OUT_OF_RANGE = "Column number out of range, should be between 0 and %s"
         const val ERROR_COLUMN_FULL = "Column %s is full"
+        const val ERROR_ALL_COLUMNS_FULL = "All columns are full"
     }
 
     private val board = Board(column = MAXIMUM_COLUMN_INDEX + 1, row = MAXIMUM_ROW_INDEX + 1)
@@ -33,19 +35,15 @@ class GameController {
         draw()
 
         do {
-            println("")
-            println("Waiting for command(type in 'h' for help):")
-
-            val input = readLine()
-            println("Input: $input")
+            var input = promptForPlayerInput()
 
             when (input) {
                 "q" -> {
                     quit = true
-                    println(QUIT_TEXT)
+                    println(TEXT_QUIT)
                 }
                 "h" -> {
-                    println(HELP_TEXT)
+                    println(TEXT_HELP)
                 }
                 else -> input?.let { performAction(it) }
             }
@@ -56,6 +54,8 @@ class GameController {
         val col = validate(input)
         updateBoard(col)
         draw()
+        updateBoardByBot()
+        draw()
     }
 
     fun validate(input: String): Int {
@@ -63,6 +63,16 @@ class GameController {
         checkIfOutOfRange(col)
         checkIfColumnFull(col)
         return col
+    }
+
+    private fun promptForPlayerInput(): String {
+        println("")
+        println("Waiting for command(type in 'h' for help):")
+
+        val input = readLine()
+        println("Input: $input")
+
+        return input ?: ""
     }
 
     private fun checkIfValidInt(input: String): Int =
@@ -87,12 +97,46 @@ class GameController {
         }
     }
 
-    private fun updateBoard(col: Int) {
+    private fun updateBoard(col: Int, isBot: Boolean = false) {
         for (row in MINIMUM_ROW_INDEX..MAXIMUM_ROW_INDEX) {
             if (board.grid[row][col] == Spot.EMPTY) {
-                board.grid[row][col] = Spot.PLAYER
+                if (isBot) {
+                    board.grid[row][col] = Spot.BOT
+                } else {
+                    board.grid[row][col] = Spot.PLAYER
+                }
+
                 return
             }
+        }
+    }
+
+    private fun updateBoardByBot() {
+        val col = pickRandomNonEmptyColumn()
+        updateBoard(col, isBot = true)
+        Thread.sleep(3000)
+    }
+
+    private fun getNonEmptyColumns(): List<Int> {
+        val nonEmptyCols = mutableListOf<Int>()
+
+        for (col in MINIMUM_COLUMN_INDEX..MAXIMUM_COLUMN_INDEX) {
+            if (board.grid[MAXIMUM_ROW_INDEX][col] == Spot.EMPTY) {
+                nonEmptyCols.add(col)
+            }
+        }
+
+        return nonEmptyCols
+    }
+
+    private fun pickRandomNonEmptyColumn(): Int {
+        val nonEmptyCols = getNonEmptyColumns()
+
+        if (nonEmptyCols.isEmpty()) {
+            throw IllegalArgumentException(ERROR_ALL_COLUMNS_FULL)
+        } else {
+            val randomIndex = (MINIMUM_COLUMN_INDEX until nonEmptyCols.size).shuffled().last()
+            return nonEmptyCols[randomIndex]
         }
     }
 }
